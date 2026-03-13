d730c4b5a8ee64b91e2dfb7128d77d549d9d244d
lucas <109983765+neyzare@users.noreply.github.com>
2026-03-13 11:39:31 +0100

    fix

--- connexion-bdd.php
+++ connexion-bdd.php
@@ -3,5 +3,5 @@ $env = parse_ini_file(__DIR__ . '/.env');
 
 define('DB_HOST',     $env['DB_HOST']);
 define('DB_NAME',     $env['DB_NAME']);
-define('DB_USER',     $env['DB_USER']);
+define('DB_USER',     $env['DB_USERNAME']);
 define('DB_PASSWORD', $env['DB_PASSWORD']);
--- controllers/page-burger-controller.php
+++ controllers/page-burger-controller.php
@@ -1,6 +1,8 @@
 <?php
 require "../connexion-bdd.php";
 
+$burgers = [];
+
 try {
     $bdd = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASSWORD);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
